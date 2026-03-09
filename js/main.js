3cdc36d80e777477131d4c7c35ab90df3c81d137
Max Bertinetti <max.bertinetti@outlook.com>
2026-03-09 08:13:04 -0300

    Alpine test

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.getElementById("menu-toggle");
-const menu = document.getElementById("mobile-menu");
-
-if (toggle && menu) {
-  toggle.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-  });
-}
